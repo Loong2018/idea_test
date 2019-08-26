947e04c4a473225c5ea3dc1f30916b9eacbced1d
conan <hzconan@163.com>
2019-08-26 14:07:28 +0800

    调整idea HTTPClient的脚本

--- manager-web/src/main/java/com/laoda/web/controller/UserController.java
+++ manager-web/src/main/java/com/laoda/web/controller/UserController.java
@@ -2,9 +2,11 @@ package com.laoda.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.laoda.pojo.Users;
 import com.laoda.service.UsersService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user")
@@ -19,4 +21,27 @@ public class UserController {
         return "ok";
     }
 
+    @RequestMapping(value = "/doGet/{userid}", method = RequestMethod.GET)
+    public String doGet(@PathVariable Integer userid) {
+        System.out.println("doGet....." + userid);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPost", method = RequestMethod.POST)
+    public String doPost(Users users) {
+        System.out.println("doPost......" + users);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doDelete/{userid}", method = RequestMethod.DELETE)
+    public String doDelete(@PathVariable Integer userid) {
+        System.out.println("doDelete......" + userid);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPut", method = RequestMethod.PUT)
+    public String doPut() {
+        System.out.println("doPut......");
+        return "ok";
+    }
 }
